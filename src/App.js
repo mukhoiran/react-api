47ae34cc068a626b22b42ec1b303b185855225bd
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2020-04-30 00:23:37 +0700

    passing data using hook with fetch api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { useState , useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [items, setItems] = useState([]);
+
+  //similar with componentDidMount()
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(data => setItems(data))
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        { items.map(
+          (item, index) => <li key={index}>{item.name}</li>
+        )}
+      </ul>
     </div>
   );
 }
